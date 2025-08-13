c3e12b3926b4068931ee7aa8c38d559a92507437
Hyunbin Kim <taque88@naver.com>
2025-08-14 04:14:01 +0900

    [SETTING] 커스텀 예외 추가

---
+++ src/main/java/com/backend/gogreen/common/exception/InternalServerException.java
@@ -0,0 +1,14 @@
+package com.backend.gogreen.common.exception;
+
+import org.springframework.http.HttpStatus;
+
+public class InternalServerException extends BaseException {
+
+    public InternalServerException() {
+        super(HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    public InternalServerException(String message) {
+        super(HttpStatus.INTERNAL_SERVER_ERROR, message);
+    }
+}
---
+++ src/main/java/com/backend/gogreen/common/exception/NotFoundException.java
@@ -0,0 +1,14 @@
+package com.backend.gogreen.common.exception;
+
+import org.springframework.http.HttpStatus;
+
+public class NotFoundException extends BaseException {
+
+    public NotFoundException() {
+        super(HttpStatus.NOT_FOUND);
+    }
+
+    public NotFoundException(String message) {
+        super(HttpStatus.NOT_FOUND, message);
+    }
+}
